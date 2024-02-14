a20fdaf497337c98617458cfb6a97df6f01337cd
MrSkitson <91326600+MrSkitson@users.noreply.github.com>
2024-02-14 11:07:09 +0000

    Refactoting

---
+++ RestTest/BaseClass.cs
@@ -0,0 +1,25 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestTest
+{
+    public class BaseClass
+    {
+        protected static RestClient _client;
+        [OneTimeSetUp]
+        public static void InitializeRestClient() =>
+            _client = new RestClient("https://api.trello.com");
+    
+        protected RestRequest RequestWithAuth(string url) =>
+           RequestWithoutAuth(url)
+               .AddQueryParameter("key", "78cc9dcd0fe568a68b5e7d8cdfab098c")
+               .AddQueryParameter("token", "ATTAa93f8c08f0535703af8d7bb92b0a2aa7b113474a74bad30fc61110df1b53b87dC13D048B");
+        protected RestRequest RequestWithoutAuth(string url) =>
+         new RestRequest(url);
+      
+    }
+}
--- RestTest/GetBoardValidationTrello - Copy.cs
+++ RestTest/GetBoardValidationTrello - Copy.cs
@@ -4,13 +4,15 @@ using RestSharp;
 using System;
 using System.Net;
 using Newtonsoft.Json.Schema;
-using System.Buffers.Text;
+
 
 namespace RestTest
 {
-    public class GetBoardTrello : BaseClass
+    public class GetBoardValidationTrello : BaseClass
     {
-       
+        //private static IRestClient _client;
+
+      
         [Test]
         public void GetBoards()
         {
@@ -18,7 +20,7 @@ namespace RestTest
                 .AddQueryParameter("field", "id, name")
                 .AddUrlSegment("member", "mr_skit");//path parametr
 
-            var response = _client.Get(request);
+            var response = ((RestClient)_client).Get(request);
 
             Console.WriteLine(response.Content);
             Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
@@ -34,7 +36,7 @@ namespace RestTest
                 .AddQueryParameter("field", "id, name")
                 .AddUrlSegment("id", "5db7e6f535458d1f8a77c0e9");
 
-            var response = _client.Get(request);
+            var response = ((RestClient)_client).Get(request);
             var responseContent = JToken.Parse(response.Content);
 
             Console.WriteLine(response.Content);
@@ -51,7 +53,7 @@ namespace RestTest
                 .AddQueryParameter("field", "id, name")
                 .AddUrlSegment("idList", "5db7e6f535458d1f8a77c0eb");
 
-            var response = _client.Get(request);
+            var response = ((RestClient)_client).Get(request);
             var responseContent = JToken.Parse(response.Content);
             Console.WriteLine(response.Content);
             Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
@@ -66,7 +68,7 @@ namespace RestTest
                 .AddQueryParameter("field", "id, name")
                 .AddUrlSegment("id", "5df0e93e13a8de6d77bd5498");
 
-            var response = _client.Get(request);
+            var response = ((RestClient)_client).Get(request);
             var responseContent = JToken.Parse(response.Content);
             Console.WriteLine(response.Content);
 
@@ -76,6 +78,37 @@ namespace RestTest
             Assert.True(responseContent.IsValid(jsonSchema));
         }
 
-        
+        [Test]
+        public async Task CheckGetBoardWithInvalidId()
+        {
+            var request = RequestWithAuth("/1/boards/{id}")
+                .AddUrlSegment("id", "invalid");
+            var response = await _client.ExecutePostAsync(request);
+            Console.WriteLine(response.Content);
+            Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
+           // Assert.AreEqual("invalid id", response.Content);
+        }
+        [Test]
+        public async Task CheckGetBoardWithInvalidAuth()
+        {
+            var request = RequestWithoutAuth("/1/boards/{id}")
+                .AddUrlSegment("id", "61fe437419cdd87656ce9fa6");
+            var response = await _client.ExecutePostAsync(request);
+            Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
+            Assert.AreEqual("Cannot POST /1/boards/61fe437419cdd87656ce9fa6", response.Content);
+        }
+
+        [Test]
+        public async Task CheckGetBoardWithAnotherUserCredentials()
+        {
+            var request =RequestWithoutAuth("/1/boards/{id}")
+                .AddQueryParameter("key", "8b32218e6887516d17c84253faf967b6")
+                .AddQueryParameter("token", "492343b8106e7df3ebb7f01e219cbf32827c852a5f9e2b8f9ca296b1cc604955")
+                .AddUrlSegment("id", "61fe437419cdd87656ce9fa6");
+            var response = await _client.ExecutePostAsync(request);
+            Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
+            //Assert.AreEqual("Cannot POST /1/boards/61fe437419cdd87656ce9fa6?", response.Content);
+        }
+
     }
 }
